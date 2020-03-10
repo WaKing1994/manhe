addc70302d9ad50adecbcd450ba37675bac3e87f
walking <WaKing1994@github.com>
2020-03-10 21:56:07 +0800

    init

--- src/main/java/com/manhe/web/controller/LoginController.java
+++ src/main/java/com/manhe/web/controller/LoginController.java
@@ -1,5 +1,8 @@
 package com.manhe.web.controller;
 
+import com.manhe.dal.dataobject.AdminDO;
+import com.manhe.service.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,6 +11,8 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class LoginController {
@@ -19,9 +24,16 @@ public class LoginController {
     @PostMapping("/admin/loginVerify")
     public String loginVerify(String username, String password, HttpSession session) {
         HttpServletRequest request = getNativeRequest();
-        session = request.getSession(true);
-        session.setAttribute("user", 12312312);
-        if (username.equals("admin")) {
+
+        Map<String, Object> param = new HashMap<>();
+        param.put("username", username);
+        param.put("password", password);
+        AdminDO adminDO = adminService.get(param);
+
+
+        if (adminDO != null) {
+            session = request.getSession(true);
+            session.setAttribute("user", adminDO);
             return "/admin/home/index";
         } else {
             return "redirect:/admin/login";
@@ -34,4 +46,6 @@ public class LoginController {
         return request;
     }
 
+    @Autowired
+    private AdminService adminService;
 }

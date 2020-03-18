a612aa8c90ebe37cc5ef6ba1605627b83fac5b29
walking <WaKing1994@github.com>
2020-03-18 22:07:07 +0800

    init

--- src/main/java/com/manhe/dal/dataobject/AdminDO.java
+++ src/main/java/com/manhe/dal/dataobject/AdminDO.java
@@ -2,7 +2,6 @@ package com.manhe.dal.dataobject;
 
 
 import java.io.Serializable;
-import java.util.Date;
 
 public class AdminDO implements Serializable {
 
@@ -10,22 +9,6 @@ public class AdminDO implements Serializable {
     private String username;
     private String phone;
     private String password;
-    private Long roleId;
-    private Date createTime;
-    private Boolean isDeleted;
-    private Date updateTime;
-
-    public AdminDO(String username, String phone, Long roleId, String password, Boolean isDeleted){
-        this.username = username;
-        this.phone = phone;
-        this.roleId = roleId;
-        this.password = password;
-        this.isDeleted = isDeleted;
-    }
-
-    public void init(){
-        this.isDeleted = false;
-    }
 
     public AdminDO() {
     }
@@ -62,36 +45,5 @@ public class AdminDO implements Serializable {
         this.password = password;
     }
 
-    public Long getRoleId() {
-        return roleId;
-    }
-
-    public void setRoleId(Long roleId) {
-        this.roleId = roleId;
-    }
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
-    public Boolean getDeleted() {
-        return isDeleted;
-    }
-
-    public void setDeleted(Boolean deleted) {
-        isDeleted = deleted;
-    }
-
-    public Date getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(Date updateTime) {
-        this.updateTime = updateTime;
-    }
 
 }

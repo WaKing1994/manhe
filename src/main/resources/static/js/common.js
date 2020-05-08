48bdff57cbf9f441b3717f26a13db448b11bee07
walking <WaKing1994@github.com>
2020-05-08 22:02:53 +0800

    init

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -135,4 +135,29 @@ marqueeVar = window.setInterval("marquee("+ i +", '"+ direction +"')",30);
 
 }
 
+function isPC() {
+	console.log("xxx");
+	var userAgentInfo = window.navigator.userAgent;
+	var Agents = [
+		"Android",
+		"iPhone",
+		"SymbianOS",
+		"Windows Phone",
+		"iPad",
+		"iPod"
+	];
+	var flag = true;
+	for (var v = 0; v < Agents.length; v++) {
+		if (userAgentInfo.indexOf(Agents[v]) > 0) {
+			flag = false;
+			break;
+		}
+	}
+	if(!flag){
+		var obj = document.getElementById('backgroundImage');
+		obj.remove();
+		var logo = document.getElementById("logoImage");
+		logo.remove();
+	}
+}
  
